e6052c26590316b778ad1839bb1d1e59d15cae2d
Thando Ncube <ncubethando20@gmail.com>
2020-08-04 21:15:06 -0400

    Adding a new header to the application

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import logoUrl from '../../public/assets/logo.svg';
 const Header = () => (
   <div className="header">
     <img src={logoUrl} alt="Tesla" />
+    <p>Designed by Thando Ncube</p>
   </div>
 )
 export default Header;
